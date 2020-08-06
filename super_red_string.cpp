84a16673b26692b252a920ad47d00b72a5050502
sridhar11-dev <52885573+sridhar11-dev@users.noreply.github.com>
2020-08-06 22:03:42 +0530

    reduced string code in c++

---
+++ super_red_string.cpp
@@ -0,0 +1,47 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    string str = "aaaabbbbcccc";
+    string o ="";
+    char temp;
+    stack<char>s;
+    for(int i=0;i<str.length();i++)
+    {
+        if(s.empty())
+        {
+            s.push(str[i]);
+        }
+        else if(s.top()==str[i])
+        {
+            s.pop();
+        }
+        else
+        {
+            s.push(str[i]);
+        }
+    }
+    
+    if(s.empty())
+    {
+        cout<<"empty string";
+    }
+    else
+    {
+        while(!s.empty())
+        {
+            o = o + s.top();
+            s.pop();
+        }
+        for(int i=0,j=o.size()-1;i<j;i++,j--)
+        {
+           temp = o[i];
+           o[i]=o[j];
+           o[j]=temp;
+        }
+    }
+    cout<<o;
+    return 0;
+}
